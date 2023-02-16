6226ccf82bcfbfcf2a7ec0340cdf6dc5d1e37c46
fer.weydson <fer.weydson@gmail.com>
2023-02-16 15:32:32 -0300

    Criação das propriedades e métodos

---
+++ Domain/Services/ServiceProduct.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct : IServiceProduct
+    {
+        private readonly IProduct _IProduct;
+
+        public ServiceProduct(IProduct IProduct)
+        {
+            _IProduct = IProduct;
+        }
+        public async Task AddProduct(Produto produto)
+        {
+            var validaNome = produto.ValidaString(produto.Nome, "Nome");
+
+            var validaValor = produto.ValidaDecimal(produto.Valor, "Valor");
+            
+            if(validaNome && validaValor)
+            {
+                produto.Estado = true;
+                await _IProduct.Add(produto);
+            }
+            
+        }
+
+        public async Task UpdateProduct(Produto produto)
+        {
+            var validaNome = produto.ValidaString(produto.Nome, "Nome");
+
+            var validaValor = produto.ValidaDecimal(produto.Valor, "Valor");
+
+            if (validaNome && validaValor)
+            {
+                await _IProduct.Update(produto);
+            }
+        }
+    }
+}
---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    internal class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base (options)
+        {
+        }
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+        private string  GetConnectionString()
+        {
+            string ConexaoPadrao = "Server=localhost\\sqlexpress; Initial Catalog=EcommerceDDD; Integrated Security=true; TrustServerCertificate=True";
+            return ConexaoPadrao;
+        }
+    }
+}
